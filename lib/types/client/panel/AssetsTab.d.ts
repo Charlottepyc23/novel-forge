4772dbd7a603b20bc6e486cee19aaa731b7eb596
watersxya <159719393+watersxya@users.noreply.github.com>
2026-08-15 13:16:49 +0800

    Add lib/types/client/panel/AssetsTab.d.ts

---
+++ lib/types/client/panel/AssetsTab.d.ts
@@ -0,0 +1,7 @@
+import type { NovelApi } from '../api.ts';
+/** Props. */
+export interface AssetsTabProps {
+    api: NovelApi;
+}
+/** 写作资产页签。 */
+export declare function AssetsTab({ api }: AssetsTabProps): import("react").JSX.Element;
